677b9f97a478a5dddeb52412f56053325ae4d790
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-09-01 11:05:20 -0400

    Some configurations

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -15,36 +15,52 @@ class QuotesSpider(scrapy.Spider):
 
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['bustillo.al17@hotmail.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'Juancho',
+        'FEED_EXPORT_ENCODING': 'utf8'
     }
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
-        
+            authors = kwargs['authors']          
+       
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        authors.extend(response.xpath('//span//small[@class="author" and @itemprop="author"]/text()').getall())
+    
+        quotes_author = [{'quote': quote, 'author': author} for  quote, author in zip(quotes, authors)]
 
         nex_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if nex_button_link:
-            yield response.follow(nex_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(nex_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
         else:
             yield {
-                'quotes': quotes
+                'quotes': quotes_author
             }            
 
     def parse(self, response):       
         title = response.xpath('//h1/a/text()').get()       
-        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()        
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()        
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        authors = response.xpath('//span//small[@class="author" and @itemprop="author"]/text()').getall()         
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()        
+
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
 
         yield {
             'title': title,            
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
-        nex_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
-        if nex_button_link:
-            yield response.follow(nex_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+        next_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_button_link:
+            yield response.follow(next_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
 
         
         # ONE PICE
