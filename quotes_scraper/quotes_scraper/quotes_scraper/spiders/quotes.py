f6f24984be8bc65d257f9b097d3b0610ac676064
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-09-01 07:48:16 -0400

    Saving data

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -15,5 +15,11 @@ class QuotesSpider(scrapy.Spider):
         title = response.xpath('//h1/a/text()').get()       
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()        
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()        
+
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
        
 
